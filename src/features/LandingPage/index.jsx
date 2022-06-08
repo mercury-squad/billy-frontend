37ceba8b925b2f83d3acf090ee1430c471184c05
KariGarcia <karengarmed@gmail.com>
2022-06-07 21:54:45 -0700

    chore: Add Material UI

--- src/features/LandingPage/index.jsx
+++ src/features/LandingPage/index.jsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
+
 const LandingPage = () => {
   return (
     <div>
       <h1>LandingPage</h1>
+      <Button variant="contained">Material UI button</Button>;
     </div>
   );
 };
