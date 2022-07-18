27e950644af9ed79257c4c691e3f2a702915a216
KariGarcia <karengarmed@gmail.com>
2022-07-18 01:52:48 -0700

    feat: add sent invoice screen

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import Welcome from 'features/welcome';
 import Dashboard from 'features/dashboard';
 import Invoices, { InvoiceForm, InvoicePreview } from 'features/invoices';
 import Projects from 'features/projects';
+import SentInvoice from 'features/invoices/sent-invoice/sent-invoice';
 
 const AppRoutes = () => {
   return (
@@ -18,7 +19,9 @@ const AppRoutes = () => {
           <Route index element={<Dashboard />} />
           <Route path="invoices" element={<Invoices />} />
           <Route path="invoices/new" element={<InvoiceForm />} />
+          <Route path="invoices/edit" element={<InvoiceForm />} />
           <Route path="invoices/:id" element={<InvoicePreview />} />
+          <Route path="invoices/sent" element={<SentInvoice />} />
           <Route path="projects" element={<Projects />} />
         </Route>
         <Route path="login" element={<Login />} />
