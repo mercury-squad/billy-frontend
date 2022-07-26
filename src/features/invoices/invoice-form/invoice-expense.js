7e858a4b62042789e6b30a08f4a32775b6d07440
KariGarcia <karengarmed@gmail.com>
2022-07-25 19:00:29 -0700

    feat: adds remove button in invoice items and expenses

--- src/features/invoices/invoice-form/invoice-expense.js
+++ src/features/invoices/invoice-form/invoice-expense.js
@@ -1,41 +1,37 @@
-import { InputLabel, InputAdornment } from '@mui/material';
+import { InputLabel, InputAdornment, IconButton } from '@mui/material';
 import Input from 'components/input';
 import { useFormContext } from 'react-hook-form';
+import { ReactComponent as DeleteIcon } from 'assets/img/delete-icon.svg';
 
-const InvoiceExpense = ({ idx }) => {
+const InvoiceExpense = ({ position, onRemove = () => {} }) => {
   const { control } = useFormContext();
 
   return (
     <div className="expense">
       <div>
         <InputLabel>Description</InputLabel>
-        <Input fullWidth control={control} rules={{ required: true }} name={`expenses.${idx}.description`} />
+        <Input fullWidth control={control} rules={{ required: true }} name={`expenses.${position}.description`} />
       </div>
-      <div>
-        <InputLabel>Quantity</InputLabel>
-        <Input
-          fullWidth
-          type="number"
-          control={control}
-          rules={{ min: 0 }}
-          InputProps={{ inputProps: { min: 0 } }}
-          name={`expenses.${idx}.quantity`}
-          placeholder="Hour or pcs"
-        />
-      </div>
-      <div>
-        <InputLabel>Amount</InputLabel>
-        <Input
-          fullWidth
-          type="number"
-          control={control}
-          rules={{ min: 0 }}
-          name={`expenses.${idx}.amount`}
-          InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
-            inputProps: { min: 0 },
-          }}
-        />
+      <div className="amount">
+        <div>
+          <InputLabel>Amount</InputLabel>
+          <Input
+            fullWidth
+            type="number"
+            control={control}
+            rules={{ required: true, min: 0 }}
+            name={`expenses.${position}.amount`}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">$</InputAdornment>,
+              inputProps: { min: 0 },
+            }}
+          />
+        </div>
+        <div className="delete-icon">
+          <IconButton onClick={() => onRemove(position)}>
+            <DeleteIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
